2a1ee9d172477e6517c31b7dd1f65ad8f3eb3b42
Maria S <mariz87@yandex.ru>
2025-11-12 15:42:01 +0300

    function randomcolor

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let colorBlock = document.querySelector('.color');
 let colorCode = document.querySelector('.code');
 let generateButton = document.querySelector('.button');
+
+function getRandomColor() {
+  let color = '#'
+  for (let i=0; i<6; i++) {
+let symbols = "0123456789ABCDEF";
+let randomIndex=Math.floor(Math.random()*16);
+color+=symbols[randomIndex];
+  }
+}
